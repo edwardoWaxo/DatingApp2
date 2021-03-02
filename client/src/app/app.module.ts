a24581431342b25706de238e827434b6914e70d0
edwardoWaxo <ed_wax@yahoo.com>
2021-03-02 08:24:50 -0700

    End of 55.58

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -8,13 +8,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
-import { HomeComponent } from './home/home.component';  
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';  
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    HomeComponent
+    HomeComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
